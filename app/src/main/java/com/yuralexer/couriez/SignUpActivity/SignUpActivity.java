2ab3c3ffeb387ed796eafd5dccccf627638a6dcc
Yuralexer <yura582005@gmail.com>
2025-05-18 22:59:22 +0300

    update: add activities

--- app/src/main/java/com/yuralexer/couriez/SignUpActivity/SignUpActivity.java
+++ app/src/main/java/com/yuralexer/couriez/SignUpActivity/SignUpActivity.java
@@ -1,4 +1,4 @@
-package com.yuralexer.couriez;
+package com.yuralexer.couriez.SignUpActivity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.yuralexer.couriez.R;
+
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
